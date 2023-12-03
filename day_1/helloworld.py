837ed76a1114da3d2c0240b0943bcc157512126d
Bamidele Olaoluwa Emmanuel <bamideleolaoluwa95@gmail.com>
2023-12-03 22:59:40 +0100

    first commit

---
+++ day_1/helloworld.py
@@ -0,0 +1,16 @@
+a = 3
+b = 4
+print (a + b)
+print (a - b)
+print(a*b)
+print(a%b)
+print(a/b)
+print(a**b)
+print(a//b)
+
+
+import math
+p = (2,3)
+q = (10, 8)
+
+print(math.dist(p,q))
---
+++ day_2/variables.py
@@ -0,0 +1,24 @@
+# Day 2: 30 Days of python programming
+firstname = 'olaoluwa'
+lastname = 'bamidele'
+fullname = 'olaoluwa bamidele'
+country = 'nigeria'
+city = 'lagos'
+age = 20
+year = 2023
+is_married = False
+is_true = True
+is_light_on = True
+first_name, last_name , fullname = 'ola', 'bamid', 'ola bamid'
+print(type(firstname));
+print(type(lastname))
+print(type(fullname))
+print(type(country))
+print(type(city))
+print(type(age))
+print(type(year))
+print(type(is_married))
+print(type(is_true))
+print(type(is_light_on))
+
+print(len(firstname))
